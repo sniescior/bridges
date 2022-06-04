e217a341617e7ff5a2679301d5ff490dae4603a8
sniescior160@gmail.com <sniescior160@gmail.com>
2022-06-04 20:47:29 +0000

    CV version init

--- sources/main.c
+++ sources/main.c
@@ -4,10 +4,13 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include "../headers/car.h"
 #include "../headers/town.h"
 #include "../headers/thread.h"
 #include "../headers/queue.h"
+
+// Condition variables version
 
 int N = 10;
 pthread_mutex_t mutex;
